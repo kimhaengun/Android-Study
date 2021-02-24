383a6b81e0cda7e17bfcaabb3ae0ed7d5b59850c
haengun <lucbo49@naver.com>
2021-02-24 09:27:47 +0900

    18.MvvM패턴 1

---
+++ MvvM-ex1/app/src/main/java/com/example/mvvmex1/model/Post.java
@@ -0,0 +1,17 @@
+package com.example.mvvmex1.model;
+
+import android.content.Intent;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Post {
+    private Integer id;
+    private String title;
+
+}
